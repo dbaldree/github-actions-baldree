8b749424de601bc14f3ca5b7bc16860509c30471
dbaldree <baldersd@hotmail.com>
2025-02-12 10:07:42 +0000

    tar-release

---
+++ actions/tar-release/entrypoint.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Create archive or exit if the command fails
+set -eu
+
+printf "\n📦 Creating tarball archive...\n"
+
+if [ "$INPUT_DIRECTORY" != "." ] 
+then
+  cd "$INPUT_DIRECTORY"
+fi
+
+if [ -z "$INPUT_EXCLUSIONS" ] 
+then
+  tar -zcvf "$INPUT_FILENAME" . || { printf "\n⛔ Unable to create %s archive.\n"; exit 1;}
+else
+  EXCLUSIONS=''
+  for EXCLUSION in $INPUT_EXCLUSIONS
+  do
+    EXCLUSIONS+=" --exclude="
+    EXCLUSIONS+=$EXCLUSION
+  done
+  tar $EXCLUSIONS -zcvf "$INPUT_FILENAME" . || { printf "\n⛔ Unable to create %s archive.\n"; exit 1;}
+fi
+
+printf "\n✔ Successfully created tarball archive.\n"
